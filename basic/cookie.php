c2539a0b335ef73036ed543609d40a9963338bc9
NominationP <zhangze1029@gmail.com>
2018-07-31 16:26:09 +0800

    git-aut-commit

--- basic/cookie.php
+++ basic/cookie.php
@@ -5,16 +5,27 @@
 	/**
 	 * client access url ==>  using setcookie() ==>
 	 *
-	 * respose : 	Server: nginx
-					Date: Tue, 31 Jul 2018 07:47:53 GMT
-					Content-Type: text/html; charset=UTF-8
-					Transfer-Encoding: chunked
-					Connection: close
-					X-Powered-By: PHP/5.6.33
-					Set-Cookie: user_name=Guru99; expires=Tue, 31-Jul-2018 07:48:53 GMT; Max-Age=60; path=/
-	 *										^
-	 *										|
-	 *	So, the brower receive this respose | and remember this cookie 
+	 * respose :    Server: nginx
+	                Date: Tue, 31 Jul 2018 07:47:53 GMT
+	                Content-Type: text/html; charset=UTF-8
+	                Transfer-Encoding: chunked
+	                Connection: close
+	                X-Powered-By: PHP/5.6.33
+	                Set-Cookie: user_name=Guru99; expires=Tue, 31-Jul-2018 07:48:53 GMT; Max-Age=60; path=/
+	 *                                      ^
+	 *                                      |
+	 *  So, the brower receive this respose | and remember this cookie 
+	 *
+	 *  When client access this url[**totally same**] will sent cookie to server
+	 *                                      |
+	 *                                      |
+	 *                                      >
+	 *
+	 * request :    GET /intertest/login1/cookies_read HTTP/1.1
+	                Cookie: my-secret-cookie=eWlibzogLLlirFkHW5ZLBxUtI0tw; user_name=Guru99
+	                Host: api.fango360.cn
+	                Connection: close
+	                User-Agent: Paw/3.1.5 (Macintosh; OS X/10.13.6) GCDHTTPRequest
 	 */
 
 
